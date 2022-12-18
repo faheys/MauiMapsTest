be34a2f1dcbe5162e28079d4d21f0d18629c71cc
Sam Fahey <Sam.Fahey@councilwise.com.au>
2022-12-19 10:06:38 +1100

    Add project files.

---
+++ MauiMapsTest/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiMapsTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MauiMapsTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiMapsTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
